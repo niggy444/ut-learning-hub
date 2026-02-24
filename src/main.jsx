5a076f917617354214d1fccf256470214341a825
niggy444 <alfbarocio@gmail.com>
2026-02-24 12:36:49 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// Este archivo conecta el HTML con tu código de React
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
